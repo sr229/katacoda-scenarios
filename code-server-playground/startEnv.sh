572e76d71647b83700bc8248755f08ffecc15daa
Kibo Hikari <enra@sayonika.moe>
2019-05-15 23:16:18 +0800

    fix(code-server-playground): fix broken shell script
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -4,18 +4,16 @@ echo "Running setup, this may take a while.";
 
 # cuRL is a massive b*tch sometimes so we'll use wget
 
-if [ ! -f "/usr/bin/wget"  ];
+if [ ! -f "/usr/bin/wget"  ]; then
   sudo apt-get install wget
 fi
-
-cd $HOME
 
 # for maintainers: please use ONLY the stable latest versions.
 
 coder_version="1.939-vsc1.33.1"
 
 
-wget -qO - "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${code_server}-linux-x64.tar.gz" | tar-xzvf && \
+wget -O - "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" | tar -xzvf && \
      cd code-server$coder_version/ && \
      chmod +x code-server && \
      code-server --allow-http --password=katacoda --port=3000
