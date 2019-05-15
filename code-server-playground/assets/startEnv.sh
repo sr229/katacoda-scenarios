a4e90a909eea1e3ad68df55f1449918577e9e84a
Kibo Hikari <enra@sayonika.moe>
2019-05-15 23:53:41 +0800

    fix: fix startEnv.sh not being the same as assets version
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/assets/startEnv.sh
+++ code-server-playground/assets/startEnv.sh
@@ -5,6 +5,8 @@ echo "Running setup, this may take a while.";
 # Ubuntu might not have net-tools, so we'll install it
 apt-get -y install net-tools
 
+mkdir projects/
+
 # cuRL is a massive b*tch sometimes so we'll use wget
 
 if [ ! -f "/usr/bin/wget"  ]; then
@@ -16,7 +18,9 @@ fi
 coder_version="1.939-vsc1.33.1"
 
 
-wget -O - "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" | tar -xzvf && \
-     cd code-server$coder_version/ && \
+wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" && \
+     tar -xzvf bin.tgz && \
+     cd code-server$coder_version-linux-x64/ && \
+     rm -rf bin.tgz && \
      chmod +x code-server && \
-     code-server --allow-http --password=katacoda --port=3000
+     ./code-server --allow-http --password=katacoda --port=3000 /root/projects
