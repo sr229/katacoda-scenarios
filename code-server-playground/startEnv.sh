9e3907fe6eee6ced6bf12fd701e26b72eb70312e
Kibo Hikari <enra@sayonika.moe>
2019-05-15 23:49:15 +0800

    fix: make code-server walk to root path
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -21,4 +21,4 @@ wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/1.939-vsc1
      cd code-server$coder_version-linux-x64/ && \
      rm -rf bin.tgz && \
      chmod +x code-server && \
-     ./code-server --allow-http --password=katacoda --port=3000
+     ./code-server --allow-http --password=katacoda --port=3000 /root
