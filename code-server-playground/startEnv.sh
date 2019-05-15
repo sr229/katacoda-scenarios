2353c68d0b2f6c1baabbc0048c83fc32a0a5ff47
Kibo Hikari <enra@sayonika.moe>
2019-05-15 23:47:30 +0800

    fix: fix broken extract and exec
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -16,7 +16,9 @@ fi
 coder_version="1.939-vsc1.33.1"
 
 
-wget -O - "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" | tar -xzvf && \
-     cd code-server$coder_version/ && \
+wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" && \
+     tar -xzvf bin.tgz && \
+     cd code-server$coder_version-linux-x64/ && \
+     rm -rf bin.tgz && \
      chmod +x code-server && \
-     code-server --allow-http --password=katacoda --port=3000
+     ./code-server --allow-http --password=katacoda --port=3000
