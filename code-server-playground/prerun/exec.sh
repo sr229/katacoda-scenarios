f2a535f6f5f71e103995284a469424ac4bedc6d7
Kibo Hikari <enra@sayonika.moe>
2019-05-15 22:39:21 +0800

    feat: add code-server playground
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

---
+++ code-server-playground/prerun/exec.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Running setup, this may take a while.";
+
+# cuRL is a massive b*tch sometimes so we'll use wget
+
+if [ ! -f "/usr/bin/wget"  ];
+  sudo apt-get install wget
+fi
+
+# for maintainers: please use ONLY the stable latest versions.
+
+coder_version="1.939-vsc1.33.1"
+
+
+wget -qO - "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${code_server}-linux-x64.tar.gz" | tar-xzvf && \
+     cd code-server$coder_version/ && \
+     chmod +x code-server && \
+     code-server --allow-http --password=katacoda --port=3000
---
+++ code-server-playground/step1.md
@@ -0,0 +1,14 @@
+Go ahead and browse the IDE at port `3000` at the terminal.
+
+We use ubuntu for this environment, so if you want to install something, for example, `build-essential`,
+do it like this:
+
+
+`sudo apt install build-essential`{{execute}}
+
+If you find any errors regarding the playground, please open an [issue](https://github.com/sr229/katacoda-scenarios/issues/new).
+
+If you find any issues with `code-server`, let the developers know at the [project repository](https://github.com/cdr/code-server/issues/new).
+
+
+Happy coding~!
