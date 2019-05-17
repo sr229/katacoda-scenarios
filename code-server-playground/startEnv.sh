e2e3984c6556c2c5a44f1cd1fc370f1f70e35533
Kibo Hikari <enra@sayonika.moe>
2019-05-17 23:44:48 +0800

    chore: optimize init scripts
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-echo "Running setup, this may take a while.";
-
-# Ubuntu might not have net-tools, so we'll install it
-apt-get -y install net-tools
-
-mkdir projects/
-
-# cuRL is a massive b*tch sometimes so we'll use wget
-
 if [ ! -f "/usr/bin/wget"  ]; then
   sudo apt-get -y install wget
 fi
@@ -16,7 +7,6 @@ fi
 # for maintainers: please use ONLY the stable latest versions.
 
 coder_version="1.939-vsc1.33.1"
-
 
 wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" && \
      tar -xzvf bin.tgz && \
--- lxd-playground/startEnv.sh
+++ lxd-playground/startEnv.sh
@@ -2,10 +2,4 @@
 
 echo "Running setup, this may take a while.";
 
-apt update && apt -y install lxd
-
-# cuRL is a massive b*tch sometimes so we'll use wget
-
-if [ ! -f "/usr/bin/wget"  ]; then
-  sudo apt-get -y install wget
-fi
+apt -y install lxd
