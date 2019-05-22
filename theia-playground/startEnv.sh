4c504de166f7dc01a6b9adf104db294d727f5d28
Kibo Hikari <enra@sayonika.moe>
2019-05-22 15:16:28 +0800

    [theia-playground] make sure theia runs a build
    Webpack is slow wtf
    
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- theia-playground/startEnv.sh
+++ theia-playground/startEnv.sh
@@ -10,6 +10,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && \
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && \
     apt-get update && \
     apt-get -qy install nodejs yarn && \
-    yarn;
+    yarn && \
+    yarn theia build;
 
 node --always-compact --max-old-space-size=64 $HOME/src-gen/backend/main.js $HOME/projects --hostname=0.0.0.0 --no-cluster
