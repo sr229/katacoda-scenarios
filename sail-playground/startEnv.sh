13351e482dc06f94bd6fbeb5d27e5731f32657a9
Kibo Hikari <enra@sayonika.moe>
2019-05-23 13:12:52 +0800

    [sail-playground] fix erronous tar command
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- sail-playground/startEnv.sh
+++ sail-playground/startEnv.sh
@@ -6,7 +6,7 @@ echo "Running setup, this may take a while.";
 # maintainers: make sure to edit this instead of the entire URL.
 sail_version="v1.0.1"
 
-wget -qO - https://github.com/cdr/sail/releases/download/$sail_version/sail-linux-amd64.tar | tar -xvf
+wget -qO - https://github.com/cdr/sail/releases/download/$sail_version/sail-linux-amd64.tar | tar -xv
 
 cd sail-linux-amd64 && \
    chmod -R 755 sail && \
