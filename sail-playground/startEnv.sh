a3b38410161c1cea8caa6613326e6bacbe19bb3e
Kibo Hikari <enra@sayonika.moe>
2019-06-22 18:58:50 +0800

    [sail-playground] upgrade to 1.0.5

--- sail-playground/startEnv.sh
+++ sail-playground/startEnv.sh
@@ -4,7 +4,7 @@ echo "Running setup, this may take a while.";
 
 
 # maintainers: make sure to edit this instead of the entire URL.
-sail_version="v1.0.1"
+sail_version="v1.0.5"
 
 wget -qO - https://github.com/cdr/sail/releases/download/$sail_version/sail-linux-amd64.tar | tar -xv
 
