f149cea81219104331f751c8bc637176ee6eaf8c
Kibo Hikari <enra@sayonika.moe>
2019-07-11 18:04:47 +0800

    [code-server-playground] use PASSWORD envVar instead
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -8,9 +8,9 @@ fi
 
 coder_version="1.1156-vsc1.33.1"
 
-wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server${coder_version}-linux-x64.tar.gz" && \
+wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/${coder_version}/code-server${coder_version}-linux-x64.tar.gz" && \
      tar -xzvf bin.tgz && \
      cd code-server$coder_version-linux-x64/ && \
      rm -rf bin.tgz && \
      chmod +x code-server && \
-     ./code-server --allow-http --password=katacoda --port=3000 /root/projects
+     PASSWORD=katacoda ./code-server --allow-http --port=3000 /root/projects
