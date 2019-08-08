c98a9760080153a07855f0dc601110e9a2bcceef
Kibo Hikari <aguilarralph13@gmail.com>
2019-08-09 02:43:33 +0800

    [code-server-playground] use lkgr builds for v2

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -10,10 +10,14 @@ NODE_ARGS="--jitless --max-semi-space-size=1 --max-old-space-size=128 --max-exec
 
 coder_version="2.preview.1-vsc1.36.1"
 
-wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/${coder_version}/code-server${coder_version}-linux-x86_64.tar.gz" && \
-     tar -xzvf bin.tgz && \
-     cd code-server$coder_version-linux-x86_64/ && \
-     rm -rf bin.tgz && \
-     chmod +x code-server && \
-     NODE_ARGS="$NODE_ARGS" PASSWORD=katacoda ./code-server --auth=password --port=3000 --host=0.0.0.0 /root/projects
- 
+# Disabled cdr builds for now because official releases are broken
+# wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/${coder_version}/code-server${coder_version}-linux-x86_64.tar.gz" && \
+#     tar -xzvf bin.tgz && \
+#     cd code-server$coder_version-linux-x86_64/ && \
+#     rm -rf bin.tgz && \
+#     chmod +x code-server && \
+#     NODE_ARGS="$NODE_ARGS" PASSWORD=katacoda ./code-server --auth=password --port=3000 --host=0.0.0.0 /root/projects
+
+wget -O - https://git.io/fj7ed | tar -xvz && \
+    cd code-server* && \
+    NODE_ARGS="$NODE_ARGS" PASSWORD=katacoda ./code-server --auth=password --port=3000 --host=0.0.0.0 /root/projects
