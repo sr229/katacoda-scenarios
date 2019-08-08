70f02fb7bf6562d3d2c0de5fec82a91976d01ab9
Kibo Hikari <aguilarralph13@gmail.com>
2019-08-08 22:15:19 +0800

    [code-server-playground] upgrade to 2.preview.1-vsc1.36.1

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -4,13 +4,16 @@ if [ ! -f "/usr/bin/wget"  ]; then
   sudo apt-get -y install wget
 fi
 
+NODE_ARGS="--jitless --max-semi-space-size=1 --max-old-space-size=128 --max-executable-size=96"
+
 # for maintainers: please use ONLY the stable latest versions.
 
-coder_version="1.1156-vsc1.33.1"
+coder_version="2.preview.1-vsc1.36.1"
 
 wget -O bin.tgz "https://github.com/cdr/code-server/releases/download/${coder_version}/code-server${coder_version}-linux-x64.tar.gz" && \
      tar -xzvf bin.tgz && \
      cd code-server$coder_version-linux-x64/ && \
      rm -rf bin.tgz && \
      chmod +x code-server && \
-     PASSWORD=katacoda ./code-server --allow-http --port=3000 /root/projects
+     NODE_ARGS="$NODE_ARGS" PASSWORD=katacoda ./code-server --auth=password --port=3000 /root/projects
+ 
