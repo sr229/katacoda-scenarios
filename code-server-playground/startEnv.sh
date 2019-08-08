c1001cf86058f6dcf897097ccdf294730099a921
Kibo Hikari <aguilarralph13@gmail.com>
2019-08-09 02:51:23 +0800

    [code-server-playground] fix missing dir

--- code-server-playground/startEnv.sh
+++ code-server-playground/startEnv.sh
@@ -4,7 +4,7 @@ if [ ! -f "/usr/bin/wget"  ]; then
   sudo apt-get -y install wget
 fi
 
-NODE_ARGS="--jitless --max-semi-space-size=1 --max-old-space-size=128 --max-executable-size=96"
+mkdir scratchpad
 
 # for maintainers: please use ONLY the stable latest versions.
 
@@ -20,4 +20,4 @@ coder_version="2.preview.1-vsc1.36.1"
 
 wget -O - https://git.io/fj7ed | tar -xvz && \
     cd code-server* && \
-    NODE_ARGS="$NODE_ARGS" PASSWORD=katacoda ./code-server --auth=password --port=3000 --host=0.0.0.0 /root/projects
+    PASSWORD=katacoda ./code-server --auth=password --port=3000 --host=0.0.0.0 /root/scratchpad
