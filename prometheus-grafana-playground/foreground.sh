fc74b89462a0807ec076998c8a86a6795160b385
Ayase Minori <chinodesuuu@gmail.com>
2024-12-06 10:57:46 +0800

    prometheus-grafana-playground: fix broken variable

--- prometheus-grafana-playground/foreground.sh
+++ prometheus-grafana-playground/foreground.sh
@@ -7,7 +7,7 @@ timeout=10
 echo "Setting up Prometheus in the background, this shell will be unusable until everything has been fully set up"
 
 while true; do
-    prometheus_response_coderesponse_code=$(curl -s -o /dev/null -w "%{http_code}" "$prometheus_url/status")
+    prometheus_response_code=$(curl -s -o /dev/null -w "%{http_code}" "$prometheus_url/status")
     grafana_response_code=$(curl -s -o /dev/null -w "%{http_code}" "$grafana_url")
 
     if [[ $prometheus_response_code -eq 200 ]] && [[ $grafana_response_code -eq 200 ]]; then
