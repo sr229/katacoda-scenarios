b78ef2ef7fb54806e52b00aa09565edf6f4ede2e
Ayase Minori <chinodesuuu@gmail.com>
2024-12-17 09:22:00 +0800

    splunk-playground: make container check take priority over health check
    The container has to exist before we can even check liveness

--- splunk-playground/foreground.sh
+++ splunk-playground/foreground.sh
@@ -5,10 +5,17 @@ timeout=10
 echo "Setting up Splunk in the background, this shell will be unusable until everything has been fully set up"
 
 while true; do
-    splunk_container_id=$(docker ps -a -q -f name="splunk")
+    if [[ -n $(docker ps -a -q -f name="splunk") ]];
+        break
+    else
+        sleep $timeout
+    fi
+done
+
+while true; do
     splunk_state=$(docker exec splunk /bin/bash -c "cat /opt/container_artifact/splunk-container.state")
 
-    if [[ -n $splunk_container_id ]] && [[ $splunk_state == "started" ]]; then
+    if [[ $splunk_state == "started" ]]; then
         echo "Splunk is ready!"
         break
     else
