11852814a77ba5c3a492ad4a0cb8321c3871a765
Ayase Minori <chinodesuuu@gmail.com>
2024-12-17 09:16:43 +0800

    splunk-playground: ensure to check docker image exists too

--- splunk-playground/foreground.sh
+++ splunk-playground/foreground.sh
@@ -4,13 +4,11 @@ timeout=10
 
 echo "Setting up Splunk in the background, this shell will be unusable until everything has been fully set up"
 
-# Wait for background script to invocate the docker command
-sleep 10
-
 while true; do
+    splunk_container_id=$(docker ps -a -q -f name="splunk")
     splunk_state=$(docker exec splunk /bin/bash -c "cat /opt/container_artifact/splunk-container.state")
 
-    if [[ $splunk_state -eq "started" ]]; then
+    if [[ -n $splunk_container_id ]] && [[ $splunk_state == "started" ]]; then
         echo "Splunk is ready!"
         break
     else
