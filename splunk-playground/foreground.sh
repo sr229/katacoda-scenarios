a989f6e3bc3dee8f9801c12f006b5f4a35ac388e
Ayase Minori <chinodesuuu@gmail.com>
2024-12-17 09:05:12 +0800

    splunk-playground: ensure to wait before checking for liveness

--- splunk-playground/foreground.sh
+++ splunk-playground/foreground.sh
@@ -4,6 +4,9 @@ timeout=10
 
 echo "Setting up Splunk in the background, this shell will be unusable until everything has been fully set up"
 
+# Wait for background script to invocate the docker command
+sleep 10
+
 while true; do
     splunk_state=$(docker exec splunk /bin/bash -c "cat /opt/container_artifact/splunk-container.state")
 
